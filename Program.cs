635162b1812071042ee24bd74799c602cec5306e
ackerley1019 <dacker35@gmail.com>
2021-02-24 00:39:33 +0800

    survey C# client solution.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System;
+using Grpc.Core;
+using Helloworld;
+
+namespace grpc_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            // using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            // Investigate: how come CalculatorClient?
+            var client = new Calculator.CalculatorClient(channel);
+
+            var reply = client.SquareRoot(
+                              new Number { value = 16.0 });
+            Console.WriteLine("SquareRoot: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- gen.sh
+++ gen.sh
@@ -1,1 +1,3 @@
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. calculator.proto
+protoc -I=. --csharp_out=. calculator.proto
+dotnet add package Grpc.Net.Client Google.Protobuf Grpc.Tools Grpc.AspNetCore
